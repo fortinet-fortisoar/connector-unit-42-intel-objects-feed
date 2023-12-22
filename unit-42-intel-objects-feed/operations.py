d9f1e73dd39e48758ea49ddf3463aae88078a7df
randhale <randhale@fortinet.com>
2023-12-22 15:31:22 +0530

    Addressed review changes

--- unit-42-intel-objects-feed/operations.py
+++ unit-42-intel-objects-feed/operations.py
@@ -1,9 +1,8 @@
 """
-  Copyright start
-  Copyright (C) 2008 - 2023 Fortinet Inc.
-  All rights reserved.
-  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
-  Copyright end
+    Copyright start
+    MIT License
+    Copyright (c) 2023 Fortinet Inc
+    Copyright end 
 """
 
 import requests
